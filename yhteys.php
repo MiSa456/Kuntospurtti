1d91378d80aab8c632fb9f789e893bc0029b3e31
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-06-18 14:49:53 +0300

    Navigoinnin muotoilua

--- yhteys.php
+++ yhteys.php
@@ -8,17 +8,18 @@
     </head>
     <body>
         <header>
-        <h1>Yhteystiedot</h1>
             <nav>
             <ul>
-                <li><a href="index.html">Etusivu</a></li>
-                <li><a href="pt.html">Personal Training</a></li>
-                <li><a href="sali.html">Kuntosalin esittely</a></li>
-                <li><a href=treenit.html>Ohjatut treenit</a></li>
+                <li><a href="index.html">Kuntospurtti</a></li>
+                <li><a href="sali.html">Kuntosali</a></li>
+                <li><a href=treenit.html>Treenit</a></li>
+                <li><a href="pt.html">Personal training</a></li>
+                <li><a href="">Yhteys</a></li>
             </ul>
             </nav>    
         </header>
         <main>
+        <h1>Yhteystiedot</h1>
             <div class="contact-laatikko">
             <div class="map">                
                 <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d820.9170644026651!2d27.86766441549097!3d70.08174651121647!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x45cb940aeef3e367%3A0x66e955beecacdb0d!2sHerrankuru%207%2C%2099990%20Utsjoki!5e0!3m2!1sfi!2sfi!4v1717869915133!5m2!1sfi!2sfi" width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></p>					
